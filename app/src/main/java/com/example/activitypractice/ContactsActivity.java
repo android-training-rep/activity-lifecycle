cf60bd5402b57f173220cdd41d4516ff4220e045
Liao Xueqing <xueqingliao@foxmail.com>
2020-07-20 21:05:00 +0800

    Get result form activity

--- app/src/main/java/com/example/activitypractice/ContactsActivity.java
+++ app/src/main/java/com/example/activitypractice/ContactsActivity.java
@@ -2,12 +2,18 @@ package com.example.activitypractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class ContactsActivity extends AppCompatActivity {
 
+    static final int REQUEST_SELECT_CONTACT = 1;
     Button jumpBtn;
 
     @Override
@@ -18,12 +24,27 @@ public class ContactsActivity extends AppCompatActivity {
         jumpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openNewPageAndGetResults();
+                selectContact();
             }
         });
     }
 
-    private void openNewPageAndGetResults() {
-        // todo: 调用系统联系人；获取返回值；显示选中的联系人
+    public void selectContact() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_CONTACT);
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK) {
+            // Get the URI and query the content provider for the phone number
+            Uri contactUri = data.getData();
+            System.out.println("-----sucuss-----");
+        }
+    }
+
 }
